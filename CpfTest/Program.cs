5bdbcef7fd48344cf00f875fc0c3be3f6a947b27
Gian Luca Da Silva Figueiredo <Gian Luca Da Silva Figueiredo>
2017-04-16 09:58:37 -0300

    Add project files.

---
+++ CpfTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CpfValidation;
+
+namespace CpfTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpf;
+            cpf = Console.ReadLine();
+            if (CpfValidationClass.Validacao(cpf) == false)
+            {
+                Console.WriteLine("CPF Invalido!");
+            }
+            else
+            {
+                Console.WriteLine("CPF Valido!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
