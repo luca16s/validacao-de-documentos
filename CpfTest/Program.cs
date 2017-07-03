eebbbb86f226171fc991e368a0fb035a916db292
luca16s <luca.gian16@outlook.com>
2017-07-02 23:21:19 -0300

    Update Program.cs file

--- CpfTest/Program.cs
+++ CpfTest/Program.cs
@@ -7,17 +7,17 @@ namespace CpfTest
     {
         static void Main(string[] args)
         {
-            string cpf;
+            string UserCpf;
             Console.WriteLine("Insira seu CPF: ");
-            cpf = Console.ReadLine();
-            if (CpfValidationClass.Validacao(cpf.Replace("-", "").Replace(".", "")) == false)
+            UserCpf = Console.ReadLine();
+            if (CpfValidationClass.Validation(UserCpf.Replace("-", "").Replace(".", "")) == false)
             {
                 Console.WriteLine("CPF Invalido!");
             }
             else
             {
                 Console.WriteLine("CPF Valido!");
-                Console.WriteLine(CpfValidationClass.EstadoCpf(cpf.Replace("-", "").Replace(".", "")));
+                Console.WriteLine(CpfRegionCheck.EstadoCpf(UserCpf.Replace("-", "").Replace(".", "")));
             }
             Console.ReadLine();
         }
